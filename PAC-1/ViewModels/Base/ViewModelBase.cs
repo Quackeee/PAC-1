a62da30bf8e7330a6b5e29c608a9fe67075686de
Quackeee <krzysztofklak7@gmail.com>
2020-04-19 15:01:19 +0200

    base class for viewmodel added

---
+++ PAC-1/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace PAC_1.ViewModels.ViewModelBase
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void onPropertyChanged(params string[] namesOfProperties)
+        {
+            if (PropertyChanged != null)
+            {
+                foreach (var prop in namesOfProperties)
+                { PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+            }
+        }
+    }
+    class RelayCommand : ICommand
+    {
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null) throw new ArgumentNullException(nameof(execute));
+            else _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute == null ? true : _canExecute(parameter);
+        public event EventHandler CanExecuteChanged
+        {
+            add { if (_canExecute != null) CommandManager.RequerySuggested += value; }
+            remove { if (_canExecute != null) CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter) { _execute(parameter); }
+    }
+}
